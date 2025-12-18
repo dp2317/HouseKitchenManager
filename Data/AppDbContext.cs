abe5ce7963cd82512f78d751ecae17944334b872
dp2317 <92938211+dp2317@users.noreply.github.com>
2025-12-18 18:28:19 -0500

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using HouseKitchenManager.Models;
 
 namespace HouseKitchenManager.Data
@@ -6,10 +6,7 @@ namespace HouseKitchenManager.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<Notification> Notifications { get; set; }
+            : base(options) { }
 
         public DbSet<Member> Members { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
