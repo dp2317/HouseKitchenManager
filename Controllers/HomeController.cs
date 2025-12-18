2bbf9ce3786c2d54621a3cc0ac86ccb635438c49
dp2317 <92938211+dp2317@users.noreply.github.com>
2025-12-18 18:26:20 -0500

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using HouseKitchenManager.Data;
-using System.Linq;
 
 namespace HouseKitchenManager.Controllers
 {
@@ -13,31 +12,30 @@ namespace HouseKitchenManager.Controllers
             _context = context;
         }
 
-       public IActionResult Index()
-{
-    var summary = _context.Members
-        .Select(m => new
+        public IActionResult Index()
         {
-            m.Id,
-            m.Name,
-            m.ColorHex,
+            var summary = _context.Members
+                .Select(m => new
+                {
+                    m.Id,
+                    m.Name,
+                    m.ColorHex,
 
-            TotalCookDays = _context.Schedules
-                .Count(s => s.MemberId == m.Id),
+                    TotalCookDays = _context.Schedules
+                        .Count(s => s.MemberId == m.Id),
 
-            AvgRating = (
-                from r in _context.Ratings
-                join s in _context.Schedules
-                    on r.ScheduleId equals s.Id
-                where s.MemberId == m.Id
-                select (double?)r.Stars
-            ).Average() ?? 0
-        })
-        .ToList();
+                    AvgRating = (
+                        from r in _context.Ratings
+                        join s in _context.Schedules
+                            on r.ScheduleId equals s.Id
+                        where s.MemberId == m.Id
+                        select (double?)r.Stars
+                    ).Average() ?? 0
+                })
+                .ToList();
 
-    ViewBag.Summary = summary;
-    return View();
-}
-
+            ViewBag.Summary = summary;
+            return View();
+        }
     }
 }
