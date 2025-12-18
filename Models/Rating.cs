fd1e18f7cd98040c123649575741806bee165817
dp2317 <92938211+dp2317@users.noreply.github.com>
2025-12-18 18:33:12 -0500

    Update Rating.cs

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+namespace HouseKitchenManager.Models;
+
 [Table("Ratings")]
 public class Rating
 {
